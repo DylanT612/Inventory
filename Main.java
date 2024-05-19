20596d18a3a9e93e65008f49aae5f06d76fbbb52
DylanT612 <168722348+DylanT612@users.noreply.github.com>
2024-05-19 00:53:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 /*
-I certify, that this computer program submitted by me is all of my own work.
-Signed: Dylan Theis 5/17/2024
 
-Author: Dylan Theis
-Date: Summer 2024
-Class: CSC322
 Project: Browsing Online Store - Main
 Description: reads items from a file and lets user browse the imported list by category
 
